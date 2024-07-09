b1b3e3dc641b254c4bc7ed933b2898559e089d05
Oscar Perez <142538269+operezcc@users.noreply.github.com>
2024-07-09 16:36:04 -0600

    go_router configurado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
