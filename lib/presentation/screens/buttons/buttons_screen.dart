3a4ca59607f9769949a6a07e38b03bb48ce304d0
Oscar Perez <142538269+operezcc@users.noreply.github.com>
2024-07-09 15:02:59 -0600

    rutas basicas configuradas

---
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ButtonsScreen extends StatelessWidget {
+  const ButtonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons screen'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
+
---
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CardsScreen extends StatelessWidget {
+  const CardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cards screen'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
+
